8fcdef8086bb82347f7ad28afa445d06f89d738a
Laura 3 <laurawmbogo@gmail.com>
2024-12-06 20:04:20 -0600

    Lesson 2 updates

---
+++ practice2_2.js
@@ -0,0 +1,7 @@
+// # Compare the values using the >, <, ===, and !== operators (answer should be False, True, False, True respectively.)
+const a = "a";
+const b = "b";
+console.log("a > b: ", ___);
+console.log("a < b: ", ___);
+console.log("a === b: ", ___);
+console.log("a !== b: ", ___);
